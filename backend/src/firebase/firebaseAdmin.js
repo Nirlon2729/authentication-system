b609d3bb9da03e27f88d57d400f8a754d1bc2270
Nirlon2729 <nirlonmacwan27@gmail.com>
2026-07-10 21:01:19 +0530

    Update Firebase Admin to use environment variables

--- backend/src/firebase/firebaseAdmin.js
+++ backend/src/firebase/firebaseAdmin.js
@@ -4,7 +4,9 @@ const { getAuth } = require("firebase-admin/auth");
 const serviceAccount = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  privateKey: process.env.FIREBASE_PRIVATE_KEY
+    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+    : undefined,
 };
 
 if (!getApps().length) {
